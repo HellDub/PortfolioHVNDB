39b9a5a8a38cce88c4f42103cfeda07857faba42
HellDub <nawakgaming.ng@gmail.com>
2023-05-10 14:20:22 +0100

    First commit of the tests branch

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card"> 
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click  on me ,the Vite and React logos to learn more
       </p>
     </>
   )
