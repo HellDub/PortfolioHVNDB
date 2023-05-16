30610d54819dfdf6ea2c752f6069779ca107436c
HellDub <nawakgaming.ng@gmail.com>
2023-05-16 14:26:52 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
+
+import Me from './components/Me'
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import Works from './components/Works'
+import  {styled}  from 'styled-components'
+
+
+const Container = styled.div `
+color: white;
+height:100vh;
+background-image: url("./images/background.jpg");
+scroll-snap-type: y mandatory;
+scroll-behavior:smooth;
+overflow-y: auto;
+scrollbar-width:0;
+&::-webkit-scrollbar{
+  display: none;
+}
+
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-     <div>
-      Hello World
-     </div>
-    </>
+  
+     <Container>
+      
+      <Hero/>
+      <Me/>
+      <Works/>
+      <Contact/>
+      
+     </Container>
+    
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+
+const Section = styled.div `
+display: flex;
+justify-content:center;
+
+
+`
+const Container = styled.div `
+width:1350px;
+display: flex;
+justify-content:space-between;
+align-items:center;
+padding:10px 0px;
+`
+const Links = styled.div `
+display: flex;
+align-items:center;
+gap:50px;
+
+
+`
+const Logo = styled.img`
+height: 50px;
+
+`
+
+const List = styled.ul `
+display: flex;
+gap: 20px;
+list-style:none;
+`
+const ListItem = styled.li `
+cursor: pointer;
+
+`
+const Button = styled.button `
+width: 100px;
+padding: 10px;
+background-color: #E5004C;
+color: #FFFFFF;
+border-radius:10px;
+cursor: pointer;
+border:none;
+font-family: 'Montserrat';
+font-weight:600;
+`
+const Icon = styled.img `
+width: 32px;
+cursor: pointer;
+`
+const Icons = styled.div `
+display: flex;
+align-items:center;
+gap:10px;
+`
+
+
+
+
+
+
+const Navbar = () => {
+  return (
+    <Section>
+        <Container>
+            <Links>
+            <Logo src="/images/Logo.svg" />
+             <List>
+              <ListItem >
+              Home
+              </ListItem>
+              <ListItem>
+                About Me
+              </ListItem>
+              <ListItem>
+                My Works
+              </ListItem>
+              <ListItem>
+                Contact 
+              </ListItem>
+             </List>
+            </Links>
+            <Icons>
+              <Icon src='/images/search.svg'/>
+              <Button>Hire Me</Button>
+            </Icons>
+        </Container>
+    </Section>
+  )
+}
+
+export default Navbar
